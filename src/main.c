969046fcd866e6f357e8319af347f102905fe200
yakshaver <775213+hidsh@users.noreply.github.com>
2025-07-13 03:17:31 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/usb/usb_device.h>
 
-void main(void)
+int main(void)
 {
     usb_enable(NULL);
 
     k_sleep(K_SECONDS(3));
 
     printk("Hello from printk over USB CDC!\n");
+
+    return 0;
 }
